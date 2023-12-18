85f53130a72ef14094b6a013d56ad375dd131353
Dmitry Chuguevskiy <75036983+bacick@users.noreply.github.com>
2023-12-18 15:44:38 +0300

    prepareing images for retina screen

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: "asset/resource",
       },
       {
